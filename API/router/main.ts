0fa8870876fc6c25473eaa845ed8cd9bae79681b
Emile ROCHE <emile.roche@ynov.com>
2020-02-10 08:21:28 +0000

    test start

--- API/router/main.ts
+++ API/router/main.ts
@@ -1,3 +1,12 @@
+const app = require('express')()
+const PORT =process.env.PORT||8080
+const urlBdd =process.env.PORT||"localhost:3306"
+app.use((req, res) =>{
+    res.end('Bonjour à tous')
+})
+app.listen(PORT, () => {
+    console.log('Serveur sur port : ', PORT)
+})
 import MongoDb from "./routes/MongoDb";
 const router = require('express').Router();
 const gameRouter = require('./routes/game.ts');
@@ -23,3 +32,4 @@ function main(host:string,urlBDD:string){
     router.use('/games', gameRouter);
     module.exports = {"router":router,"bddPlayers":bddPlayer};
 }
+main("localhost",urlBdd);
